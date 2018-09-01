6536a00da5aa15def146759ceb23a8c670380dad
Dustin Sallings <dustin@spy.net>
2018-08-31 20:33:08 -0700

    Functioning 1wire stuff with temp fetching.

--- 1wire.ino
+++ 1wire.ino
@@ -1,36 +1,115 @@
 #include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ADDR_LEN 8
 
 class OneWireProtocol : public Protocol {
 public:
-    OneWireProtocol(WiFiClient &c) : Protocol("1wire", c), ds(1) {}
+    OneWireProtocol(WiFiClient &c) : Protocol("1wire", c), ds(D6) {}
 
-    void go() {
-        ds.reset_search();
-        byte data[12];
-        byte addr[8];
-
-        if (!ds.search(addr)) {
-            client.println("No more addresses.");
-            ds.reset_search();
-            delay(1000);
-            return;
-        }
-
-        client.print("ROM =");
-        for (int i = 0; i < 8; i++) {
-            client.write(' ');
+    void printAddr() {
+        for (int i = 0; i < ADDR_LEN; i++) {
+            if (addr[i] < 0x10) {
+                client.print("0");
+            }
             client.print(addr[i], HEX);
         }
+        client.println("");
+    }
 
+    void list() {
+        ds.reset_search();
+        int found = 0;
+
+        while (ds.search(addr)) {
+            if (OneWire::crc8(addr, 7) != addr[7]) {
+                client.println("ERR: CRC is not valid!");
+                return;
+            }
+
+            printAddr();
+            found++;
+        }
+
+        if (found == 0) {
+            client.println("ERR:  no devices found");
+            return;
+        } else {
+            client.print("found ");
+            client.println(found);
+        }
+
+        ds.reset_search();
+        ds.reset();
+    }
+
+    int readHalfHex() {
+        const char hexdigits[] = "0123456789abcdef";
+        const char HEXDIGITS[] = "0123456789ABCDEF";
+        int c = client.read();
+        for (int i = 0; i < 16; i++) {
+            if (c == hexdigits[i] || c == HEXDIGITS[i]) {
+                return i;
+            }
+        }
+
+        client.print("ERR: invalid hex digit: ");
+        client.println(c);
+
+        return -1;
+    }
+
+    int readHexDigit() {
+        int c1 = readHalfHex();
+        if (c1 == -1) {
+            return -1;
+        }
+        int c2 = readHalfHex();
+        if (c1 == -1) {
+            return -1;
+        }
+        return (c1 << 4) | c2;
+    }
+
+    void select() {
+        for (int i = 0; i < ADDR_LEN; i++) {
+            int d = readHexDigit();
+            if (d == -1) {
+                client.println("ERR: read hex digits");
+                return;
+            }
+            addr[i] = (byte)d;
+        }
         if (OneWire::crc8(addr, 7) != addr[7]) {
-            client.println("CRC is not valid!");
+            client.println("ERR: CRC is not valid!");
+        }
+        ds.select(addr);
+    }
+
+    void temp() {
+        DallasTemperature sensors(&ds);
+        sensors.requestTemperatures();
+        client.println(sensors.getTempC(addr));
+    }
+
+    void go() {
+        if (!client.available()) {
             return;
         }
-        ds.reset();
+        char c = client.read();
+        switch (c) {
+        case -1:
+            return;
+        case 'l': list(); break;
+        case 's': select(); break;
+        case 't': temp(); break;
+        }
     }
 
 private:
     OneWire ds;
+    byte data[12];
+    byte addr[ADDR_LEN];
 };
 
 Protocol *newOneWireProtocol(WiFiClient client) {
